ca5357b969751e9896675630f7100c50773cadf7
mandip <shresthamandip12@gmail.com>
2020-08-10 13:05:51 +0545

    Econd commit

--- lib/nav/login.dart
+++ lib/nav/login.dart
@@ -1,25 +1,70 @@
 import 'package:flutter/material.dart';
 
 import 'drawer.dart';
-class LoginScreen extends StatelessWidget{
+class LoginScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() =>_LoginScreenState(); //this
+  //or State<StatefulWidget> createState(){  return _LoginScreenState(); }its same
+
+}
+class _LoginScreenState extends State<LoginScreen>{
+  String _email , _password;
+  final GlobalKey<FormState>_formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
+    return SafeArea( child: Scaffold(
+      resizeToAvoidBottomPadding: false,
+      backgroundColor: Color(0xfff2f3f7),
         appBar: AppBar(
           title: Text('Login'),
 
         ),
         drawer: MainDrawer(),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              Text('We are in Login Screen')
-            ],
-          ),
+       body: Form(
+         key: _formkey,
+         child: Column(
+           children: <Widget>[
+             TextFormField(
+                validator:(input){
+                  // ignore: missing_return
+                  if(input.isEmpty){
 
-        )
+                    return "Please type an email";
+                  }
+                },
+               onSaved:(input)=> _email = input ,
+               decoration: InputDecoration(labelText: 'Email'),
+
+             ),
+             TextFormField(
+               // ignore: missing_return
+               validator:(input){
+                 if(input.isEmpty){
+                   return 'Please type an password';
+                 }
+               },
+               onSaved:(input)=> _password= input ,
+               decoration: InputDecoration(labelText: 'password'),
+               obscureText: true,
+
+             ),
+             RaisedButton(
+               onPressed: (){},
+               child: Text('Login'),
+             )
+
+           ],
+         ),
+       ),
+    ),
     );
+  }
+  void signIn(){
+    final FormState formState  = _formkey.currentState;
+    if(formState.validate()){
+
+    }
   }
 
 }
